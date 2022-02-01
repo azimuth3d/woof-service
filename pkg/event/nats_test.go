326472cd29a404a8b2aede727739b527f11ac844
azimuth3d <top.khomkrit@godigit.net>
2022-02-01 17:57:31 +0700

    Remove unnecessary test

--- pkg/event/nats_test.go
+++ pkg/event/nats_test.go
@@ -15,6 +15,8 @@ package event
 
 import (
 	"testing"
+
+	"github.com/nats-io/nats.go"
 )
 
 func TestDefaultConnection(t *testing.T) {
@@ -25,36 +27,28 @@ func TestDefaultConnection(t *testing.T) {
 	nc.Close()
 }
 
-// func TestDefaultConnection(t *testing.T) {
-// 	s := RunDefaultServer()
-// 	defer s.Shutdown()
+func TestConnectionStatus(t *testing.T) {
+	s := RunDefaultServer()
+	defer s.Shutdown()
 
-// 	nc := NewDefaultConnection(t)
-// 	nc.Close()
-// }
+	nc := NewDefaultConnection(t)
+	defer nc.Close()
 
-// func TestConnectionStatus(t *testing.T) {
-// 	s := RunDefaultServer()
-// 	defer s.Shutdown()
+	if nc.Status() != nats.CONNECTED || nc.Status().String() != "CONNECTED" {
+		t.Fatal("Should have status set to CONNECTED")
+	}
 
-// 	nc := NewDefaultConnection(t)
-// 	defer nc.Close()
-
-// 	if nc.Status() != nats.CONNECTED || nc.Status().String() != "CONNECTED" {
-// 		t.Fatal("Should have status set to CONNECTED")
-// 	}
-
-// 	if !nc.IsConnected() {
-// 		t.Fatal("Should have status set to CONNECTED")
-// 	}
-// 	nc.Close()
-// 	if nc.Status() != nats.CLOSED || nc.Status().String() != "CLOSED" {
-// 		t.Fatal("Should have status set to CLOSED")
-// 	}
-// 	if !nc.IsClosed() {
-// 		t.Fatal("Should have status set to CLOSED")
-// 	}
-// }
+	if !nc.IsConnected() {
+		t.Fatal("Should have status set to CONNECTED")
+	}
+	nc.Close()
+	if nc.Status() != nats.CLOSED || nc.Status().String() != "CLOSED" {
+		t.Fatal("Should have status set to CLOSED")
+	}
+	if !nc.IsClosed() {
+		t.Fatal("Should have status set to CLOSED")
+	}
+}
 
 // func TestConnClosedCB(t *testing.T) {
 // 	s := RunDefaultServer()
